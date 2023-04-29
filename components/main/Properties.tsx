2f627b7c80f108dd8b48019da810cc2c325d0891
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-04-29 17:11:29 +0430

    overflow fixed again

--- components/main/Properties.tsx
+++ components/main/Properties.tsx
@@ -4,7 +4,7 @@ import PropertyCard from "./PropertyCard";
 const Properties = () => {
   return (
     <>
-      <div className=" property-container h-[630px] grid grid-cols-4 max-md:flex-col gap-4 px-2 pb-20 max-md:grid-cols-1 max-md:overflow-y-scroll">
+      <div className=" property-container grid grid-cols-4 max-md:flex-col gap-4 px-2 pb-20 max-md:grid-cols-1 max-md:overflow-y-scroll">
         <PropertyCard />
         <PropertyCard />
         <PropertyCard />
