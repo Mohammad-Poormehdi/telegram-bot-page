ec38b267207d47ebf570b9181f58d93c9bed4b51
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-04-30 17:46:26 +0430

    SEO optimized

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
-import Glass from "@/components/Glass"
-import Background from "@/components/background/Background"
-import TextSection from "@/components/main/TextSection"
-import Navbar from "@/components/navbar/Navbar"
+import Glass from "@/components/Glass";
+import Background from "@/components/background/Background";
+import TextSection from "@/components/main/TextSection";
+import Navbar from "@/components/navbar/Navbar";
+import Head from "next/head";
 
-const Home = ()=>{
-  return <div className="inset-0 overflow-hidden h-screen relative p-20 flex justify-center items-center max-md:p-5">
-    <Background />
-    <Glass />
-  </div>
-}
-export default Home
+const Home = () => {
+  return (
+    <>
+    <Head>
+      <title>سایت</title>
+      <meta name="description" content="سایت" />
+    </Head>
+      <div className="inset-0 overflow-hidden h-screen relative p-20 flex justify-center items-center max-md:p-5">
+        <Background />
+        <Glass />
+      </div>
+    </>
+  );
+};
+export default Home;
